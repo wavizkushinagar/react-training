f46d8952c9816ce82302b2f2f343f79fc7fd0bfa
dubeygyan <gyanendra.waviz@gmail.com>
2022-04-06 16:57:09 +0530

    login-page

--- Rahul/slotmachine/src/Images.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Images = (props) => {
-    return  <img src={props.imgsrc} alt="myPic" className="card_img" />;
-   
-};
-export default Images;
--- admin-dashbord/rahul/src/Components/BodyComponent/NotificationComponant.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function NotificationComponant() {
-  return (
-    <div>NotificationComponant</div>
-  )
-}
